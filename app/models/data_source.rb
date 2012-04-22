8ba7e50bffcc4762961dc0eb785356f82d9d712c
Matt Harris <matt@themattharris.com>
2012-04-21 18:18:40 -0700

    Find datasources by tag or category

--- app/models/data_source.rb
+++ app/models/data_source.rb
@@ -3,4 +3,23 @@ class DataSource < ActiveRecord::Base
   has_many :tags, :through => :data_source_tags
   has_many :data_source_categories
   has_many :categories, :through => :data_source_categories
+
+  class << self
+    def tagged_with(tags)
+      data_sources = []
+      Tag.find_all_by_slug(tags).each do |t|
+        data_sources+=t.data_sources
+      end
+      data_sources
+    end
+
+    def in_category(category)
+      data_sources = []
+      Category.find_all_by_slug(category).each do |t|
+        data_sources+=t.data_sources
+      end
+      data_sources
+    end
+  end
+
 end
