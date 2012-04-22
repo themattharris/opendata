675093d6c1a6344b56f3d95e6e74c109a1838951
Matt Harris <matt@themattharris.com>
2012-04-22 11:52:13 -0700

    move into default paths

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def has_tags?
+    Tag.count > 0
+  end
+
+  def has_categories?
+    Category.count > 0
+  end
+
+  def has_curators?
+    true
+  end
 end
